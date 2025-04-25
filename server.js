b71ae292aec5e37e4cc79b51dd1e4a4d7b7cf25a
oliver <121308232+olivery4@users.noreply.github.com>
2025-04-25 15:19:08 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,11 +13,30 @@ app.use(bodyParser.json()); // Parse JSON bodies
 // Example POST endpoint (e.g. to handle form submissions)
 app.post('/submit', (req, res) => {
   const data = req.body;
+  saveData(data);
   console.log('Form data received:', data);
-
-  // Respond with success
-  res.status(200).json({ message: 'Form submitted successfully!' });
 });
+
+async function saveData(data) {
+  const client = new MongoClient("mongodb://localhost:27017");
+  
+  try {
+    await client.connect();
+
+    const dataBase = client.db("olivery4Data");
+    const collection = dataBase.collection("formData");
+
+    const result = await collection.insertOne({ data: data });
+
+    console.log("data saved: ", result);
+    console.log("collection: ", collection);
+  } catch (error) {
+    console.log("error: ", error);
+  } finally {
+    await client.close();
+  }
+  
+};
 
 // Start server
 app.listen(PORT, () => {
